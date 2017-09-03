7162598285ddd419b3e00edce50096f2c07b1c12
clover <clover.hsc@gmail.com>
2017-09-03 17:33:06 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './websocket.service';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ WebsocketService, ChatService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  private message = {
+    author: 'Clover',
+    message: 'this is a test message'
+  };
+
+  constructor(private chatService: ChatService) {
+    chatService.messages.subscribe(
+      (msg) => {
+        console.log(msg);
+        console.log(`Response from websocket: ${msg}`); }
+    );
+  }
+  ngOnInit() {
+
+  }
+
+  sendMsg () {
+    console.log('new message from client to websocket: ', this.message);
+    this.chatService.messages.next(this.message);
+    this.message.message = '';
+  }
 }
